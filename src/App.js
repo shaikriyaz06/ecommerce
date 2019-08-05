ffdf633f7ffdb5fc646344fb0cabff92640a4340
shaikriyaz06 <shaikriyaz222@gmail.com>
2019-08-06 04:16:04 +0530

    pushing entire project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+// import logo from './logo.svg';
+import './App.css';
+import Homepage from './pages/homepage/homepage.component'
+
+function App() {
+  return (
+    <Homepage></Homepage>
+    )
+}
+
+export default App;
